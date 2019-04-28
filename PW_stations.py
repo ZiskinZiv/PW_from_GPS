4d796840d1892f58fa613d4ab9b8b8bd52943fd5
shlomi ziskin ziv <shlomiziskin@gmail.com>
2019-04-28 16:03:48 +0300

    added filtering procedures plotting etc..

--- PW_stations.py
+++ PW_stations.py
@@ -15,7 +15,8 @@ if sys.platform == 'linux':
 elif sys.platform == 'darwin':  # mac os
     work_path = '/Users/shlomi/Documents/PW_yuval/'
 PW_stations_path = work_path + '1minute/'
-
+stations = pd.read_csv('stations.txt', header=0, delim_whitespace=True,
+                       index_col='NAME')
 
 def proc_1minute(path):
     stations = pd.read_csv(path + 'Zstations', header=0,
@@ -65,26 +66,74 @@ def read_stations_to_dataset(path, group_name='israeli', save=False,
         for filename in file_list_with_path:
             st_name = filename.split('/')[-1].split('.')[0].split('_')[-1]
             print('Reading station {}'.format(st_name))
-            stations.append(xr.open_dataarray(filename))
-        # ds = xr.merge(stations)
+            da = xr.open_dataarray(filename)
+            da = da.dropna('time')
+            stations.append(da)
+        ds = xr.merge(stations)
     if save:
-        savefile = 'garner_' + group_name+ '_stations.nc'
+        savefile = 'garner_' + group_name + '_stations.nc'
         print('saving {} to {}'.format(savefile, path))
         ds.to_netcdf(path + savefile, 'w')
         print('Done!')
-    return stations    
+    return ds
 
-  
-def overlap_time_xr(*args):
-    """return the intersection of datetime objects from time field in *args"""
-    # caution: for each arg input is xarray with dim:time
-    time_list = []
-    for ts in args:
-        time_list.append(ts.time.values)
-    intersection = set.intersection(*map(set, time_list))
-    intr = sorted(list(intersection))
-    return intr
 
+def filter_stations(path, group_name='israeli', save=False):
+    """filter bad values in trop products stations"""
+    import xarray as xr
+    filename = 'garner_' + group_name + '_stations'
+    print('Reading {}.nc from {}'.format(filename, path))
+    ds = xr.open_dataset(path + filename + '.nc')
+    ds['zwd'].attrs['units'] = 'Zenith Wet Delay in cm'
+    stations = [x for x in ds.data_vars.keys()]
+    for station in stations:
+        print('filtering station {}'.format(station))
+        # first , remove negative values:
+        ds[station] = ds[station].where(ds[station].sel(zwd='value') > 0)
+        # get zscore of data:
+        zscore = Zscore_xr(ds[station].sel(zwd='value'), dim='time')
+        ds[station] = ds[station].where(np.abs(zscore) < 5)
+    if save:
+        filename = filename + '_filtered.nc'
+        print('saving {} to {}'.format(filename, path))
+        comp = dict(zlib=True, complevel=9)  # best compression
+        encoding = {var: comp for var in ds.data_vars}
+        ds.to_netcdf(path + filename, 'w', encoding=encoding)
+        print('Done!')
+    return ds
+
+#def overlap_time_xr(*args, union=False):
+#    """return the intersection of datetime objects from time field in *args"""
+#    # caution: for each arg input is xarray with dim:time
+#    time_list = []
+#    for ts in args:
+#        time_list.append(ts.time.values)
+#    if union:
+#        union = set.union(*map(set, time_list))
+#        un = sorted(list(union))
+#        return un
+#    else:
+#        intersection = set.intersection(*map(set, time_list))
+#        intr = sorted(list(intersection))
+#        return intr
+
+
+def plot_stations_on_map(path, stations_df):
+    import geopandas as gpd
+    isr = gpd.read_file(path +'israel_demog2012.shp')
+    stations = gpd.GeoDataFrame(stations_df,
+                                geometry=gpd.points_from_xy(stations_df.LON,
+                                                            stations_df.LAT))
+    ax=isr.plot()
+    gdf.plot(ax=ax,color='red')
+    for x, y, label in zip(stations_df.LON, stations_df.LAT, stations_df.index):
+        ax.annotate(label, xy=(x, y), xytext=(3, 3), textcoords="offset points")
+
+def Zscore_xr(da, dim='time'):
+    """input is a dattarray of data and output is a dattarray of Zscore
+    for the dim"""
+    z = (da - da.mean(dim=dim)) / da.std(dim=dim)
+    return z
 
 def desc_nan(data, verbose=True):
     """count only NaNs in data and returns the thier amount and the non-NaNs"""
