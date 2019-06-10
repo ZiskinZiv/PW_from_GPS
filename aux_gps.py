34cfd8ba60b1293b35e4d85e717d85645c897625
shlomi ziskin ziv <shlomiziskin@gmail.com>
2019-06-10 18:27:43 +0300

    added auxiliarry subroutines

---
+++ aux_gps.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 14:33:19 2019
+
+@author: ziskin
+"""
+
+
+def dim_intersection(da_list, dim='time', dropna=True):
+    import pandas as pd
+    if dropna:
+        setlist = [set(x.dropna(dim)[dim].values) for x in da_list]
+    else:
+        setlist = [set(x[dim].values) for x in da_list]
+    empty_list = [x for x in setlist if not x]
+    if empty_list:
+        print('NaN dim drop detected, check da...')
+        return
+    u = list(set.intersection(*setlist))
+    # new_dim = list(set(a.dropna(dim)[dim].values).intersection(
+    #     set(b.dropna(dim)[dim].values)))
+    if dim == 'time':
+        new_dim = sorted(pd.to_datetime(u))
+    else:
+        new_dim = sorted(u)
+    return new_dim
+
+
+def get_unique_index(da, dim='time'):
+    import numpy as np
+    _, index = np.unique(da[dim], return_index=True)
+    da = da.isel({dim: index})
+    return da
+
+
+def Zscore_xr(da, dim='time'):
+    """input is a dattarray of data and output is a dattarray of Zscore
+    for the dim"""
+    z = (da - da.mean(dim=dim)) / da.std(dim=dim)
+    return z
+
+
+def desc_nan(data, verbose=True):
+    """count only NaNs in data and returns the thier amount and the non-NaNs"""
+    import numpy as np
+    import xarray as xr
+
+    def nan_da(data):
+        nans = np.count_nonzero(np.isnan(data.values))
+        non_nans = np.count_nonzero(~np.isnan(data.values))
+        if verbose:
+            print(str(type(data)))
+            print(data.name + ': non-NaN entries: ' + str(non_nans) + ' of total ' +
+                  str(data.size) + ', shape:' + str(data.shape) + ', type:' +
+                  str(data.dtype))
+            print('Dimensions:')
+        dim_nn_list = []
+        for dim in data.dims:
+            dim_len = data[dim].size
+            dim_non_nans = np.int(data.dropna(dim)[dim].count())
+            dim_nn_list.append(dim_non_nans)
+            if verbose:
+                print(dim + ': non-NaN labels: ' +
+                      str(dim_non_nans) + ' of total ' + str(dim_len))
+        return non_nans
+    if isinstance(data, xr.DataArray):
+        nn_dict = nan_da(data)
+        return nn_dict
+    elif isinstance(data, np.ndarray):
+        nans = np.count_nonzero(np.isnan(data))
+        non_nans = np.count_nonzero(~np.isnan(data))
+        if verbose:
+            print(str(type(data)))
+            print('non-NaN entries: ' + str(non_nans) + ' of total ' +
+                  str(data.size) + ', shape:' + str(data.shape) + ', type:' +
+                  str(data.dtype))
+    elif isinstance(data, xr.Dataset):
+        for varname in data.data_vars.keys():
+            non_nans = nan_da(data[varname])
+    return non_nans
