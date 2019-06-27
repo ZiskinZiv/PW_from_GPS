a7614a4be0250b2952881508cb2f943d0595ba71
shlomi ziskin ziv <shlomiziskin@gmail.com>
2019-06-27 16:44:29 +0300

    finally fixed import path problems. added detecting downloaded files

--- ims_download_all_script.py
+++ ims_download_all_script.py
@@ -5,17 +5,35 @@ Created on Tue Jun 25 14:29:10 2019
 
 @author: ziskin
 """
+from PW_startup import *
 from ims_procedures import ims_api_get_meta
 from ims_procedures import download_ims_single_station
 from pathlib import Path
-savepath = Path('/home/ziskin/Work_Files/PW_yuval/IMS_T')
+savepath = Path('/home/ziskin/Work_Files/PW_yuval/IMS_T/10mins')
 
 
 def download_all_10mins_ims(savepath):
+    def get_already_dl_dict(savepath):
+        import collections
+        dl_files = []
+        for file in savepath.rglob('*.nc'):
+            dl_files.append(file.as_posix().split('/')[-1])
+        already_dl_id = [int(x.split('_')[1]) for x in dl_files]
+        already_dl_names = [x.split('_')[0] for x in dl_files]
+        already_dl = dict(zip(already_dl_id, already_dl_names))
+        od = collections.OrderedDict(sorted(already_dl.items()))
+        return od
+
     stations = ims_api_get_meta(active_only=True, channel_name='TD')
     for index, row in stations.iterrows():
-        download_ims_single_station(savepath=savepath, channel_name='TD',
-                                    stationid=row['stationId'])
+        st_id = row['stationId']
+        od = get_already_dl_dict(savepath)
+        if st_id not in od.keys():
+            download_ims_single_station(savepath=savepath, channel_name='TD',
+                                        stationid=st_id)
+        else:
+            print('station {} is already in {}, skipping...'.format(od[st_id],
+                  savepath))
     return
 
 download_all_10mins_ims(savepath)
