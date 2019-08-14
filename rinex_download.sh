411d166ca523da6e461ab9b3ddc9d56f2630fe15
shlomi ziskin ziv <shlomiziskin@gmail.com>
2019-08-14 17:58:34 +0300

    added rinex download script

---
+++ rinex_download.sh
@@ -0,0 +1,12 @@
+nohup python -u single_download.py --path ~/Work_Files/PW_yuval/rinex_from_garner/ --mode rinex --station t &>nohup_t_download.txt&
+nohup python -u single_download.py --path ~/Work_Files/PW_yuval/rinex_from_garner/ --mode rinex --station e &>nohup_e_download.txt&
+nohup python -u single_download.py --path ~/Work_Files/PW_yuval/rinex_from_garner/ --mode rinex --station l &>nohup_l_download.txt&
+nohup python -u single_download.py --path ~/Work_Files/PW_yuval/rinex_from_garner/ --mode rinex --station a &>nohup_a_download.txt&
+nohup python -u single_download.py --path ~/Work_Files/PW_yuval/rinex_from_garner/ --mode rinex --station 1 &>nohup_1_download.txt&
+nohup python -u single_download.py --path ~/Work_Files/PW_yuval/rinex_from_garner/ --mode rinex --station 4 &>nohup_4_download.txt&
+nohup python -u single_download.py --path ~/Work_Files/PW_yuval/rinex_from_garner/ --mode rinex --station 9 &>nohup_9_download.txt&
+nohup python -u single_download.py --path ~/Work_Files/PW_yuval/rinex_from_garner/ --mode rinex --station 0 &>nohup_0_download.txt&
+nohup python -u single_download.py --path ~/Work_Files/PW_yuval/rinex_from_garner/ --mode rinex --station . &>nohup_._download.txt&
+nohup python -u single_download.py --path ~/Work_Files/PW_yuval/rinex_from_garner/ --mode rinex --station 1 &>nohup_1_download.txt&
+nohup python -u single_download.py --path ~/Work_Files/PW_yuval/rinex_from_garner/ --mode rinex --station 9 &>nohup_9_download.txt&
+nohup python -u single_download.py --path ~/Work_Files/PW_yuval/rinex_from_garner/ --mode rinex --station d &>nohup_d_download.txt&
