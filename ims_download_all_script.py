fac5b22006415998856be723aa55acfa623d39f9
shlomi ziskin ziv <shlomiziskin@gmail.com>
2019-12-02 17:36:50 +0200

    added download single station and logger

--- ims_download_all_script.py
+++ ims_download_all_script.py
@@ -5,8 +5,8 @@ Created on Tue Jun 25 14:29:10 2019
 
 @author: ziskin
 """
-# TODO: add logger 
 # TODO: add option to just update recent data from 10 mins api
+
 
 def check_path(path):
     import os
@@ -36,9 +36,184 @@ def generate_delete(savepath, channel_name):
     return
 
 
+def download_ims_single_station(stationid, savepath=None,
+                                channel_name='TD', update=None):
+    """download single station with channel_name from earliest to latest.
+    if chanel_name is None, download all channels"""
+    import requests
+    import pandas as pd
+    import logging
+
+    def parse_ims_to_df(raw_data, ch_name):
+        """gets ims station raw data, i.e., r.json()['data'] and returns
+        a pandas dataframe"""
+        import pandas as pd
+        if ch_name is not None:
+            datetimes = [x['datetime'] for x in raw_data]
+            data = [x['channels'][0] for x in raw_data]
+            df = pd.DataFrame.from_records(data,
+                                           index=pd.to_datetime(datetimes,
+                                                                utc=True))
+            df.drop(['alias', 'description'], axis=1, inplace=True)
+            cols = [ch_name + '_' + x for x in df.columns]
+            df.columns = cols
+        elif ch_name is None:
+            # add all channels d/l here:
+            datetimes = [x['datetime'] for x in raw_data]
+            names = [x['name'] for x in data['channels']]
+            keys = [*data['channels'][0].keys()]
+        return df
+
+    def to_dataarray(df, meta):
+        # add all channels d/l here:
+        import pandas as pd
+        ds = df.to_xarray()
+        ds['time'] = pd.to_datetime(ds.time)
+        channel_name = [*ds.data_vars.keys()][0].split('_')[0]
+        channel_id = ds[channel_name + '_id'].isel(time=0).values.item()
+        to_drop = [x for x in ds.data_vars.keys() if 'value' not in x]
+        ds = ds.drop(to_drop)
+        da = ds[channel_name + '_value'].reset_coords(drop=True)
+        da.name = meta['name']
+        da.attrs['channel_id'] = int(channel_id)
+        da.attrs['channel_name'] = channel_name
+        da.attrs['station_name'] = meta['name']
+        da.attrs['station_id'] = meta['id']
+        da.attrs['active'] = meta['active']
+        da.attrs['station_lat'] = str(meta['loc']['latitude'])
+        da.attrs['station_lon'] = str(meta['loc']['longitude'])
+        for key, value in da.attrs.items():
+            print(key, value)
+        return da
+
+    def get_dates_list(start_date, end_date):
+        """divide the date span into full 1 years and a remainder, tolist"""
+        import numpy as np
+        import pandas as pd
+        end_date = pd.to_datetime(end_date)
+        start_date = pd.to_datetime(start_date)
+        s_year = start_date.year
+        e_year = end_date.year
+        years = np.arange(s_year, e_year + 1)
+        dates = [start_date.replace(year=x) for x in years]
+        if (end_date - dates[-1]).days > 0:
+            dates.append(end_date)
+        return dates
+
+    logger = logging.getLogger('ims_downloader')
+    myToken = 'f058958a-d8bd-47cc-95d7-7ecf98610e47'
+    headers = {'Authorization': 'ApiToken ' + myToken}
+    r = requests.get('https://api.ims.gov.il/v1/envista/stations/',
+                     headers=headers)
+    stations_10mins = pd.DataFrame(r.json())
+    meta = {}
+    st_name = stations_10mins['name'].where(
+            stations_10mins['stationId'] == stationid).dropna()
+    location = stations_10mins['location'].where(
+            stations_10mins['stationId'] == stationid).dropna()
+    active = stations_10mins['active'].where(
+            stations_10mins['stationId'] == stationid).dropna()
+    meta['name'] = '-'.join(st_name.iloc[0].split(' '))
+    meta['id'] = stationid
+    meta['loc'] = location.iloc[0]
+    meta['active'] = active.iloc[0]
+    r_early = requests.get('https://api.ims.gov.il/v1/envista/stations/' +
+                           str(stationid) + '/data/earliest', headers=headers)
+    r_late = requests.get('https://api.ims.gov.il/v1/envista/stations/' +
+                          str(stationid) + '/data/latest', headers=headers)
+    data = r_early.json()['data'][0]
+    if update is not None:
+        earliest = update + pd.Timedelta(10, unit='m')
+    else:
+        earliest = pd.to_datetime(data['datetime']).strftime('%Y-%m-%d')
+    data = r_late.json()['data'][0]
+    latest = pd.to_datetime(data['datetime']).strftime('%Y-%m-%d')
+    logger.info(
+         'Downloading station {} with id: {}, from {} to {}'.format(
+                 st_name,
+                 stationid,
+                 earliest,
+                 latest))
+    # one channel download:
+    if channel_name is not None:
+        channel_id = [x['id'] for x in data['channels']
+                      if x['name'] == channel_name]
+        if channel_id:
+            logger.info('getting just {} channel with id: {}'.format(channel_name,
+                                                                     channel_id[0]))
+            ch_id = channel_id[0]
+            dates = get_dates_list(earliest, latest)
+            df_list = []
+            for i in range(len(dates) - 1):
+                first_date = dates[i].strftime('%Y/%m/%d')
+                last_date = dates[i + 1].strftime('%Y/%m/%d')
+                logger.info('proccesing dates: {} to {}'.format(first_date,
+                                                                last_date))
+                dl_command = ('https://api.ims.gov.il/v1/envista/stations/' +
+                              str(stationid) + '/data/' + str(ch_id) +
+                              '?from=' + first_date + '&to=' + last_date)
+                r = requests.get(dl_command, headers=headers)
+                if r.status_code == 204:  # i.e., no content:
+                    logger.warning('no content for this search, skipping...')
+                    continue
+                logger.info('parsing to dataframe...')
+                df_list.append(parse_ims_to_df(r.json()['data'], channel_name))
+            logger.info('concatanating df and transforming to xarray...')
+            df_all = pd.concat(df_list)
+            # only valid results:
+            # df_valid = df_all[df_all['valid']]
+            df_all.index.name = 'time'
+            # remove duplicated index values:
+            df_all = df_all[~df_all.index.duplicated()]
+            first = df_all.index[0]
+            last = df_all.index[-1]
+            new_index = pd.date_range(first, last, freq='10min')
+            df_all = df_all.reindex(new_index)
+            valid_name = channel_name + '_valid'
+            value_name = channel_name + '_value'
+            df_all[valid_name].fillna(False, inplace=True)
+            # replace non valid measurments with nans
+            new_vals = df_all[value_name].where(df_all[valid_name])
+            df_all[value_name] = new_vals
+            df_all.index.name = 'time'
+            da = to_dataarray(df_all, meta)
+            if update is not None:
+                return da
+            else:
+                filename = '_'.join([meta['name'], str(meta['id']), channel_name,
+                                     '10mins']) + '.nc'
+                comp = dict(zlib=True, complevel=9)  # best compression
+                encoding = {var: comp for var in da.to_dataset().data_vars}
+                logger.info('saving to {} to {}'.format(filename, savepath))
+                da.to_netcdf(savepath / filename, 'w', encoding=encoding)
+                # print('done!')
+    # all channels download add support here:
+    elif channel_name is None:
+        logger.info('getting all channels...')
+        dates = get_dates_list(earliest, latest)
+        df_list = []
+        for i in range(len(dates) - 1):
+            first_date = dates[i].strftime('%Y/%m/%d')
+            last_date = dates[i + 1].strftime('%Y/%m/%d')
+            logger.info('proccesing dates: {} to {}'.format(first_date,
+                                                            last_date))
+            dl_command = ('https://api.ims.gov.il/v1/envista/stations/' +
+                          str(stationid) + '/data?from=' + first_date +
+                          '&to=' + last_date)
+            r = requests.get(dl_command, headers=headers)
+            if r.status_code == 204:  # i.e., no content:
+                logger.warning('no content for this search, skipping...')
+                break
+            logger.info('parsing to dataframe...')
+            df_list.append(parse_ims_to_df(r.json()['data'], None))
+    return
+
+
 def download_all_10mins_ims(savepath, channel_name='TD'):
     from aux_gps import path_glob
     import xarray as xr
+    import logging
+    logger = logging.getLogger('ims_downloader')
     glob = '*_{}_10mins.nc'.format(channel_name)
     files = sorted(path_glob(savepath, glob, return_empty_list=True))
     files = [x for x in files if x.is_file()]
@@ -64,12 +239,12 @@ def download_all_10mins_ims(savepath, channel_name='TD'):
                                  '10mins']) + '.nc'
             comp = dict(zlib=True, complevel=9)  # best compression
             encoding = {var: comp for var in da.to_dataset().data_vars}
-            print('saving to {} to {}'.format(filename, savepath))
+            logger.info('saving to {} to {}'.format(filename, savepath))
             da.to_netcdf(savepath / filename, 'w', encoding=encoding)
-            print('done!')
+            # print('done!')
         else:
-            print('station {} is already in {}, skipping...'.format(st_id,
-                  savepath))
+            logger.warning('station {} is already in {}, skipping...'.format(st_id,
+                                                                             savepath))
     return
 
 
@@ -77,8 +252,9 @@ if __name__ == '__main__':
     import argparse
     import sys
     from ims_procedures import ims_api_get_meta
-    from ims_procedures import download_ims_single_station
     from pathlib import Path
+    from aux_gps import configure_logger
+    logger = configure_logger('ims_downloader')
     channels = ['BP', 'DiffR', 'Grad', 'NIP', 'Rain', 'RH', 'STDwd', 'TD',
                 'TDmax', 'TDmin', 'TG', 'Time', 'WD', 'WDmax', 'WS', 'WS10mm',
                 'WS1mm', 'WSmax']
