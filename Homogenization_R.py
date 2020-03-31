7c2428255c8d00aca546d38df6903930a275a9bd
shlomi <shlomiziskin@gmail.com>
2020-03-31 17:10:58 +0300

    added read_dat_file

--- Homogenization_R.py
+++ Homogenization_R.py
@@ -5,11 +5,6 @@ Created on Sun Mar 29 17:04:31 2020
 
 @author: shlomi
 """
-from PW_paths import work_yuval
-import os
-from aux_gps import path_glob
-os.environ['R_HOME'] = '/home/shlomi/anaconda3/lib/R'
-
 #def download_packages():
 #    from rpy2.robjects.packages import importr
 #    import rpy2.robjects.packages as rpackages
@@ -20,7 +15,55 @@ os.environ['R_HOME'] = '/home/shlomi/anaconda3/lib/R'
 #    rm.install_github('ECCM-CDAS/RHtests/V4_files')
 
 
-def export_pw_station_to_csv(pw_da, savepath=work_yuval):
+def read_dat_file(loadpath, station='tela', sample='monthly'):
+    import pandas as pd
+    from aux_gps import path_glob
+    file = path_glob(
+        loadpath, '{}_{}_means_out_*.dat'.format(station, sample))[0]
+    df = pd.read_csv(
+        file,
+        header=None,
+        delim_whitespace=True,
+        na_values="-999.00")
+    df.columns = [
+        'ind',
+        'date',
+        station,
+        'trend_shift',
+        'mean_adjusted',
+        '{}_anom'.format(station),
+        'anom_trend_shift',
+        'seasonal_trend_shift',
+        'QM_adj',
+        'anom_trend_no_shift']
+    df['date'] = df['date'].astype(str)
+    if sample == 'monthly':
+        df['date']=df['date'].str[:6]
+        df['date'] = pd.to_datetime(df['date'], format='%Y%m')
+    else:
+        df['date'] = pd.to_datetime(df['date'], format='%Y%m%d')
+    df = df.set_index(df['date'])
+    df = df.drop(['ind', 'date'], axis=1)
+    return df
+
+
+def export_all(loadpath, savepath, sample='MS'):
+    import xarray as xr
+    pw = xr.load_dataset(loadpath / 'GNSS_PW_thresh_50.nc')
+    if sample == 'MS':
+        print('Monthly means selected:')
+    elif sample == '1D':
+        print('Daily means selected:')
+    pw = pw[[x for x in pw.data_vars if '_error' not in x]]
+    pw = pw.resample(time=sample).mean()
+    for da in pw.data_vars:
+        print('exporting {} station to csv'.format(da.upper()))
+        _ = export_pw_station_to_csv(pw[da], savepath=savepath)
+    print('Done!')
+    return
+
+
+def export_pw_station_to_csv(pw_da, savepath):
     import pandas as pd
     name = pw_da.name
     df = pw_da.to_dataframe()
@@ -44,22 +87,61 @@ def export_pw_station_to_csv(pw_da, savepath=work_yuval):
     return df
 
 
-import rpy2.robjects as robjects
-from rpy2.robjects.packages import importr
-from rpy2.rinterface import RRuntimeWarning
-base = importr('base')
-base.warnings()
-r = robjects.r
-r.source('RHtests.R')
-monthly_files = path_glob(work_yuval, 'PW_*_monthly_means_for_RHtests.csv')
-mm_file = "/mnt/DATA/Work_Files/PW_yuval/PW_tela_monthly_means_for_RHtests.csv"
-# now r.FindU and other functions working
-out="/mnt/DATA/Work_Files/PW_yuval/out/"
-try:
-    o = r.FindU(
-            InSeries="/mnt/DATA/Work_Files/PW_yuval/InFile.csv",
-            output="/mnt/DATA/Work_Files/PW_yuval/Out",
-            MissingValueCode="-999.00",
-            Mq=12)
-except RRuntimeWarning as e:
-    print(e)
+def run_RH_tests(station='tela', path=None, sample='monthly'):
+    import rpy2.robjects as robjects
+    from pathlib import Path
+    #from rpy2.robjects.packages import importr
+    #from rpy2.rinterface import RRuntimeWarning
+    #base = importr('base')
+    #base.warnings()
+    r = robjects.r
+    r.source('RHtests.R')
+    in_file = "{}/PW_{}_{}_means_for_RHtests.csv".format(
+        path.as_posix(), station, sample)
+    if not Path(in_file).is_file():
+        print('{} not found ...\n pls run export_pw_station_to_csv or export_all'.format(in_file))
+        return
+    # now r.FindU and other functions working
+    out = "{}/{}_{}_means_out".format(path.as_posix(), station, sample)
+    print('running FindU')
+    r.FindU(InSeries=in_file,
+            output=out,
+            MissingValueCode="-999.00")
+    print('running StepSize')
+    r.StepSize(InSeries=in_file, output=out, MissingValueCode="-999.00",
+               InCs=out + '_mCs.txt')
+    return
+
+
+if __name__ == '__main__':
+    import argparse
+    import sys
+    from pathlib import Path
+    from aux_gps import configure_logger
+    from PW_paths import work_yuval
+    import os
+    homo_path = work_yuval / 'homogenization'
+    os.environ['R_HOME'] = '/home/shlomi/anaconda3/lib/R'
+    logger = configure_logger('RH_tests')
+    savepath = Path(homo_path)
+    parser = argparse.ArgumentParser(description='a command line tool for running the RHtests climatology homogenization procedures.')
+    optional = parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
+    # remove this line: optional = parser...
+    required.add_argument('--station', help="GNSS 4 letter station", type=str)
+    
+    #optional.add_argument('--station', nargs='+',
+    #                      help='GPS station name, 4 UPPERCASE letters',
+    #                      type=check_station_name)
+#                          metavar=str(cds.start_year) + ' to ' + str(cds.end_year))
+#    optional.add_argument('--half', help='a spescific six months to download,\
+#                          e.g, 1 or 2', type=int, choices=[1, 2],
+#                          metavar='1 or 2')
+    parser._action_groups.append(optional)  # added this line
+    args = parser.parse_args()
+    # print(parser.format_help())
+#    # print(vars(args))
+    if args.station is None:
+        print('station is a required argument, run with -h...')
+        sys.exit()
+    run_RH_tests(args.station, path=savepath, sample='monthly')
