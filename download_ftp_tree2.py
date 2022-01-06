1bd1b2cb2504aa344828536f2838c95813e972a5
shlomi <shlomiziskin@gmail.com>
2022-01-06 11:12:35 +0200

    removed old files

--- download_ftp_tree2.py
+++
@@ -1,6 +0,0 @@
-import ftplib
-from ftptool import FTPHost
-ftp = ftplib.FTP("garner.ucsd.edu", 'anonymous', "shlomiziskin@gmail.com")
-a_host=FTPHost(ftp)
-a_host.mirror_to_local('/pub/solutions/gipsy/trop', '/home/shlomi/Desktop/DATA/Work_Files/PW_yuval/garner/trop')
-
