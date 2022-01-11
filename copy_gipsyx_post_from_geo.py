348258d5922eb8dadf029bd51048c4e26a3b712b
shlomi <shlomiziskin@gmail.com>
2022-01-11 09:18:11 +0200

    added PWV copy as well

--- copy_gipsyx_post_from_geo.py
+++ copy_gipsyx_post_from_geo.py
@@ -124,6 +124,12 @@ def copy_post_from_geo(remote_path, station, days):
                 print('{} final solution not found in {}'.format(curr_sta,
                       src_rigid_path))
                 continue
+            try:
+                filepaths = path_glob(src_rigid_path, '*PWV*.nc')
+            except FileNotFoundError:
+                print('{} final solution not found in {}'.format(curr_sta,
+                      src_rigid_path))
+                continue
             for filepath in filepaths:
                 filename = filepath.as_posix().split('/')[-1]
                 print(filename)
